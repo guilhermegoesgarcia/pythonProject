dcb8729a1f175201738a45c5d6fb1995a52c3136
guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>
2021-03-16 16:52:28 -0300

    Initial commit
    Signed-off-by: guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>

--- Game_Adivinha.py
+++ Game_Adivinha.py
@@ -1,3 +1,38 @@
-''' Nesse projeto vamos criar um jogo de adivinhação '''
+''' Nesse projeto vamos criar um jogo de adivinhação'''
+# O jogador deve adivinhar um nomero secreto!
+#
 
+print('\n**********************************')
 print('Bem vindo ao Game de Adivinhação!')
+print('**********************************', end='\n\n')
+
+# numero para ser descoberto
+numero_secreto = 42
+
+# laço
+total_tentativas = 3
+
+for rodada in range(1, total_tentativas + 1):
+    chute = int(input('Digite o seu numero no console: '))
+    print(f'Você digitou {chute}')
+
+    # condiçoes
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    print(f"Tentativa {rodada} de {total_tentativas}")
+    if (acertou):
+        print('\nVocê Acertou!! *_*\n')
+        rodada = total_tentativas
+    else:
+        if maior:
+            print('\nVocê errou ! =/ ')
+            print('     O numero chutado foi MAIOR que o Numero Secreto \n')
+        elif menor:
+            print('\nVocê errou ! =/ ')
+            print('  O numero chutado foi MENOR que o Numero Secreto \n')
+
+print('************************')
+print("     FIM DE JOGO !!")
+print('************************')
