9c32e441415e67fe52856a8dc06fd0bba588a909
guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>
2021-03-17 11:52:10 -0300

    Initial commit
    Signed-off-by: guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>

--- Game_Forca.py
+++ Game_Forca.py
@@ -6,12 +6,23 @@ def jogar_forca():
     print('   Bem vindo ao Game da Forca !')
     print('**********************************', end='\n\n')
 
-    palavra_secreta = 'banana'
+    palavra_secreta = 'python'
     enforcouo = False
     acertou = False
 
     # enquanto não enforcou e não acertou__
     while (not enforcouo and not acertou):
+
+        chute = input('Qual letra?')
+
+        # tratamento da entrada (retirada de espaços )
+        chute = chute.strip()
+        index = 0
+
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print('Letra ({}) encontrada na posição ({})'.format(letra, index))
+            index += 1
         print("Jogando..")
 
     print('************************')
