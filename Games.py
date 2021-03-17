f19b3c8d9093cc713c84b24f72c79c7210aaf393
guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>
2021-03-16 22:53:42 -0300

    Initial commit
    Signed-off-by: guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>

--- Games.py
+++ Games.py
@@ -1,19 +1,20 @@
 import Game_Adivinha as Ad
 import Game_Forca as Fo
 
-print('\n**********************************')
-print('******* Escolha seu Jogo! ********')
-print('**********************************', end='\n\n')
+def escolhe_jogo():
+    print('\n**********************************')
+    print('******* Escolha seu Jogo! ********')
+    print('**********************************', end='\n\n')
 
-print('''
-(1) -> Game_Forca
-(2) -> Game_Adivinha
-''')
-jogo = int(input('Qual Jogo? '))
+    print('''
+    (1) -> Game_Forca
+    (2) -> Game_Adivinha
+    ''')
+    jogo = int(input('Qual Jogo? '))
 
-if (jogo == 1):
-    print('Carregando Game_Forca', end='\n')
-    Fo.jogar_forca()
-elif (jogo == 2):
-    print('Carregando Game_Adivinha', end='\n')
-    Ad.jogar_adivinha()
+    if (jogo == 1):
+        print('Carregando Game_Forca', end='\n')
+        Fo.jogar_forca()
+    elif (jogo == 2):
+        print('Carregando Game_Adivinha', end='\n')
+        Ad.jogar_adivinha()
