0640aa5d3c635a3d46c894a74ba88735a126bd96
guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>
2021-03-16 22:23:06 -0300

    Initial commit
    Signed-off-by: guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>

--- Games.py
+++ Games.py
@@ -1,3 +1,6 @@
+import Game_Adivinha as Ad
+import Game_Forca as Fo
+
 print('\n**********************************')
 print('******* Escolha seu Jogo! ********')
 print('**********************************', end='\n\n')
@@ -10,7 +13,7 @@ jogo = input('Qual Jogo? ')
 
 if (jogo == 1):
     print('Carregando Game_Forca', end='\n')
-    jogar_forca()
+    Fo.jogar_forca()
 elif (jogo == 2):
     print('Carregando Game_Adivinha', end='\n')
-    jogar_adivinha()
+    Ad.jogar_adivinha()
