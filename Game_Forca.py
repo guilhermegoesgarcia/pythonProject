5f522d57f7d878d101dbf11c2c6b17bdeae69295
guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>
2021-03-17 09:51:53 -0300

    Initial commit
    Signed-off-by: guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>

--- Game_Forca.py
+++ Game_Forca.py
@@ -1,7 +1,18 @@
+'''Nesse projeto vamos criar um jogo da Forca, para adivinhar palavras'''
+
+
 def jogar_forca():
     print('\n**********************************')
     print('   Bem vindo ao Game da Forca !')
     print('**********************************', end='\n\n')
+
+    palavra_secreta = 'banana'
+    enforcouo = False
+    acertou = False
+
+    # enquanto não enforcou e não acertou__
+    while (not enforcouo and not acertou):
+        print("Jogando..")
 
     print('************************')
     print("     FIM DE JOGO !!")
