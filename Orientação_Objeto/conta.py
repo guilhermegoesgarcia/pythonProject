f856d962e2536a0257c0e76731069dee090b777e
guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>
2021-03-18 18:26:11 -0300

    Projeto Orientado a Objeto

--- Orientação_Objeto/conta.py
+++ Orientação_Objeto/conta.py
@@ -3,7 +3,36 @@ class Conta:
 
     def __init__(self,numero, titular, saldo, limite):
         print('Construindo Objeto ...{}'.format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+         print('\nSaldo de {} do Títular {}'.format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor,destino):
+        self.saca(valor)
+        destino.deposita(valor)
+        self.extrato()
+        destino.extrato()
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self,limite):
+        self.__limite = limite
