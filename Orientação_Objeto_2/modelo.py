933faf95b42a20bff67564403cb6c173386a18c3
guilhermegoesgarcia <63941611+guilhermegoesgarcia@users.noreply.github.com>
2021-03-26 15:29:25 -0300

    Orientação_Objeto_2 (continuação) bora Laaa

--- Orientação_Objeto_2/modelo.py
+++ Orientação_Objeto_2/modelo.py
@@ -26,24 +26,51 @@ class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
-
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - Duração:{self.duracao}- LIKES:{self.likes}'
 
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporada):
         super().__init__(nome, ano)
         self.temporada = temporada
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - Temporada:{self.temporada}- LIKES:{self.likes}'
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
 
+    def tamanho(self):
+        return len(self.programas)
 
 
 atlanta = Serie('Atlanta', 2018 , 2)
 vingadores = Filme('vingadores-guerra infinita', 2018, 160)
+tmep = Filme ('Todo mundo em panico', 1999, 100)
+demolidor =  Filme('demolidor', 2016, 2)
 
 vingadores.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 
 print()
-print(f'{vingadores.nome} - {vingadores.ano} - {vingadores.duracao} - {vingadores.likes}')
-print(f'{atlanta.nome} - {atlanta.ano} - {atlanta.temporada} - {atlanta.likes}')
+
+
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist ('fim de semana', filmes_e_series)
+
+for programa in playlist_fim_de_semana.programas:
+    print(programa)
+
